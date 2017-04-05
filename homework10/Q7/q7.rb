c019315e83376875e88948cfb45407c2d419413b
vianali <vian.ali47@gmail.com>
2017-04-05 16:32:22 +0000

    editing q7

--- homework10/Q7/q7.rb
+++ homework10/Q7/q7.rb
@@ -1,24 +1,25 @@
-b = [5, 10, 23, 10, 7, 9, 39, 16, 1, 12, 36, 20, 50]
-c = Array.new(b.length)     
+ b = [5, 10, 23, 10, 7, 9, 39, 16, 1, 12, 36, 20, 50];
+ c = Array.new(b.length);      
 
 
-i=0 
+i=0
 while i<c.length
 c[i]=b[i]*(0.667)+(0.5)
-puts c[i]
+puts (c[i])
 i=i+1
 end
 
-puts ('______________')
+puts ('______________');
 
-i=0 
+
+sum=0;
+i=0
 while i<c.length
-sum=0
-j=0
-while j<c.length
-sum=sum+b[i]*c[j]
-puts sum
-j=j+1
+sum=sum+c[i]
 i=i+1
 end
+j=0 
+while j<b.length
+puts (sum*b[j])
+j=j+1
 end
