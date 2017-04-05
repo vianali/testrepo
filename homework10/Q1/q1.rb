753cf7ca2734ed50d59ac2d633fb8e9a80b1a552
vianali <vian.ali47@gmail.com>
2017-04-05 20:25:52 +0000

    editing q1

--- homework10/Q1/q1.rb
+++ homework10/Q1/q1.rb
@@ -1,22 +1,21 @@
-var a = ["john", "mike", "susan", "matt", "joe", "larry", "ken", "samantha", "jenny", "Sara", "henry"];
+a = ["john", "mike", "susan", "matt", "joe", "larry", "ken", "samantha", "jenny", "Sara", "henry"]
+
+def capitalize_words(string)
+  words = string.split(" ")
+  i = 0
+  while i < words.length
+    words[i] = words[i].upcase
+  i = i + 1
+  end
+  return words.join(" ")
+end
+
 
 j=0
 while j<a.length
- word1=a[j]
- word2=""
 if ((j+1)%3==0)
-    i = 0
-    while i< word1.length
- aChar = word1.charCodeAt(i);
-    if (65 <= aChar && aChar<=90)
-    aChar =aChar + 32
+puts capitalize_words(a[j])
 end
-    word2=word2+word1.charAt(i).toUpperCase();
- end
- i=i+1
- end
-puts ( word2);
- end
 j=j+1
 end
 
